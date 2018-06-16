c696c87a8e98b2d7203eeb525ac2f8f2eb8f2ffa
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-16 17:53:53 +0300

    Added a DevConfig subclass

--- app/views.py
+++ app/views.py
@@ -7,8 +7,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
-    
+    message='News Highlight'
+    return render_template('index.html', message = message)
+
 @app.route('/news/<int:news_id>')
 def news(news_id):
 
