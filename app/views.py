1fa5721668fd944daa1b5ab3c1a90359627ca09e
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-19 11:00:29 +0300

    Added a file

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+
 # Views
 @app.route('/')
 def index():
@@ -7,13 +8,22 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message='News Highlight'
-    return render_template('index.html', message = message)
+    message ='News Highlight'
+    return render_template('index.html',message = message)
 
-@app.route('/news/<int:news_id>')
+@app.route('/news/<news_id>')
 def news(news_id):
 
     '''
-    View news page function that returns the news details page and its data
+    View movie page function that returns the news details page and its data
     '''
     return render_template('news.html',id = news_id)
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)
