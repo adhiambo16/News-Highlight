dbbd3465d76d84593a7ca3d27e9fb922723fd16e
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-19 11:33:02 +0300

    Added a file

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    MOVIE_API_BASE_URL ='https://newsapi.org/v2/top-headlines/everything?apiKey={58ffdf91c72b4fccb8d745405b739de3'
 
 
 
@@ -24,15 +24,11 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
- DEBUG = True
+    DEBUG = True
 
 
-    class Config:
-
-    '''
-    General configuration parent class
-    '''
-    MOVIE_API_BASE_URL =https://newsapi.org/v2/top-headlines/everything?apiKey=58ffdf91c72b4fccb8d745405b739de3
+    
+  
 
    
 
