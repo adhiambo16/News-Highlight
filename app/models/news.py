2cfa1837292650f7fc97e38085135b91ce0abc4c
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-19 12:16:11 +0300

    Defined news class

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define Movie Objects
+    '''
+
+    def __init__(self,id,name,url,category):
+        self.id =id
+        self.name = name
+        self.url = "http://abcnews.go.com"
+        self.category = general
+    
