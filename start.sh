671ac35fd3e4ad20f572f36a276cbeb8250f43b6
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-19 15:30:06 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-export MOVIE_API_KEY=<Your api key>
-export SECRET_KEY=<Your secret key>
+export MOVIE_API_KEY='58ffdf91c72b4fccb8d745405b739de3
+'
+
+export SECRET_KEY='secret'
 
 python3.6 manage.py server
